e2cefb1cc64033e13ccfa69486c59466585732c7
alex jr <alexsandropimenta16@gmail.com>
2024-10-13 22:51:38 -0300

    alex

--- js/carrossel.js
+++ js/carrossel.js
@@ -10,31 +10,6 @@ $(document).ready(function () {
       });
 
 
-        // $('.apartamento').slick({
-        //   dots: true,
-        //   arrows: false,
-        //   autoplay: true,
-        //   infinite: true,
-        //   speed: 500,
-        //   fade: true,
-        //   cssEase: 'linear'
-        // });
-
-      
-        // $('.apartamento').slick({
-        //   slidesToShow: 1,
-        //   slidesToScroll: 1,
-        //   arrows: false,
-        //   fade: true,
-        //   asNavFor: '.imagens'
-        // });
-        // $('.imagens').slick({
-        //   slidesToShow: 3,
-        //   slidesToScroll: 1,
-        //   asNavFor: '.apartamento',
-        //   dots: true,
-        //   centerMode: true,
-        //   focusOnSelect: true
-        // });
+     
      
 });
